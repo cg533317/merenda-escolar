bb5a6d81ea9cfcd2295a60e3df8a4fece611f7ac
Carlos Gomes <cg533317@gmail.com>
2026-08-20 18:34:18 -0300

    feat: adicionar servico base da aplicacao

---
+++ backend/services/app_service.py
@@ -0,0 +1,13 @@
+﻿from backend.config import Config
+
+
+class AppService:
+    """Serviços relacionados às informações da aplicação."""
+
+    @staticmethod
+    def get_info():
+        return {
+            "name": Config.APP_NAME,
+            "environment": Config.APP_ENV,
+            "debug": Config.DEBUG
+        }
---
+++ tests/test_app_service.py
@@ -0,0 +1,9 @@
+﻿from backend.services.app_service import AppService
+
+
+def test_get_info():
+    info = AppService.get_info()
+
+    assert info["name"] == "AquaBot"
+    assert info["environment"] == "development"
+    assert info["debug"] is True
